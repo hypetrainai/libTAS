58bd1408155f815eaa071bbd3059efddaa4b159f
Jonathan Shen <jonathanasdf@hotmail.com>
2019-04-20 17:55:03 -0700

    Log thread id with messages.

--- src/shared/sockethelpers.cpp
+++ src/shared/sockethelpers.cpp
@@ -24,6 +24,7 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <sys/un.h>
+#include <thread>
 #include <iostream>
 #include <vector>
 
@@ -114,7 +115,7 @@ void sendSharedConfig(const SharedConfig* elem)
 
 void sendMessage(int message)
 {
-    printf("Send %s\n", MESSAGE_NAMES[message].c_str());
+    printf("(%ld) Send %s\n", std::this_thread::get_id(), MESSAGE_NAMES[message].c_str());
     sendData(&message, sizeof(int));
 }
 
@@ -147,7 +148,7 @@ int receiveMessage()
     int ret = receiveInt(&msg);
     if (ret < 0)
         return ret;
-    printf("Recv %s\n", MESSAGE_NAMES[msg].c_str());
+    printf("(%ld) Recv %s\n", std::this_thread::get_id(), MESSAGE_NAMES[msg].c_str());
     return msg;
 }
 
